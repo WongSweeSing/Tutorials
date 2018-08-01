53cdc48ae2a15d796b1e4454facd31fd6f03443f
WongSweeSing <wongsweesing@gmail.com>
2018-08-01 09:26:46 +0800

    define findMax as a function

--- findMax.py
+++ findMax.py
@@ -1,21 +1,31 @@
 # findMax.py
-# To find the maximim value in an array
+# To find the maximim value in an array.
 # input: An array of number
 # output : the maximum number in the array. If there multiple numbers with the same
 # maximum value. It will just choose of them
 
-anArray = [239, 25, 30.5, 1, 8]
 
-i = 0
-j = 0 
-arrayLength = len(anArray)
+def findMax(anArray):
+      
+        """This function finds the maximum value in an array.
+        input: An array of number
+        output : the maximum number in the array. If there multiple numbers with the same
+        maximum value. It will just choose of them. """
 
-while j < arrayLength:
-	if anArray[i] < anArray[j]:
-		i = j
-	j += 1
+        i = 0
+        j = 0
+        arrayLength = len(anArray)
+
+        while j < arrayLength:
+                if anArray[i] < anArray[j]:
+                        i = j
+                j += 1
 	
-print("The maximum value in array is {}".format(anArray[i]))
+        print("The maximum value in array is {}".format(anArray[i]))
 	
+anArray = [239, 250, 30.5, 1, 1008]
 
+findMax(anArray)
+
+input("\n\nPress the enter key to exit.")
 	
