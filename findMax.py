fb0f023cca7e44616fc31f5bf97d99224ccb6128
WongSweeSing <wongsweesing@gmail.com>
2018-08-04 20:28:27 +0800

    Replace for loop with list comprehension

--- findMax.py
+++ findMax.py
@@ -12,11 +12,10 @@ def findMax(file_location, file_name):
         input: A text file with numbers. First line of file is a header and can be skipped
         output : the maximum number in the array. If there multiple numbers with the same
         maximum value. It will just choose one of them. """
-        anArray = [] # array for storing the numbers read from text file
+
         with open(os.path.join(file_location, file_name)) as f: # f is a file handler
                 next(f) # first line is a header, can skip this line
-                for line in f: 
-                        anArray.append(float(line)) # storing the number read from text file to array anArray
+                anArray = [float(line) for line in f] # using list comprehension to store numbers from txt file to anArry
                         
         i = 0
         j = 0
@@ -32,7 +31,7 @@ def findMax(file_location, file_name):
 	
                 return print("The maximum value in array is {}".format(anArray[i]))
 	
-#anArray = [239, 250, 30.5, 1, 1008]
+#anArray = [239, 250, 30.5, 1, 100]
 #anArray = [-1]
 # file_location = "/Users/JosephineWong/Desktop/Python Programming for Absolute Beginner/How_to_solve_it_by_computer"
 # file_name = "numbers.txt"
